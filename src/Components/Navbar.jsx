656bd08ae72670e1006ddbf69468627a9065d738
mupashirbare <mubashirbarre12@gmail.com>
2025-02-22 06:34:34 -0800

    i was apdated the setup and added navbar.

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Navbar({ cartCount }) {
+  return (
+    <nav className="bg-teal-600 text-white p-4 flex justify-between items-center">
+      <Link to="/" className="text-lg font-bold">E-Commerce</Link>
+      <Link to="/cart" className="relative">
+        <FaShoppingCart size={24} />
+        {cartCount > 0 && (
+          <span className="absolute -top-2 -right-2 bg-red-500 text-xs rounded-full px-2 py-1">
+            {cartCount}
+          </span>
+        )}
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
