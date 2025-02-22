a301ad60e9265d4db20a7e6944280e0046d301eb
mupashirbare <mubashirbarre12@gmail.com>
2025-02-22 06:36:39 -0800

    i was added the [200~import ProductCard from ./ProductCard;.

---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,14 @@
+import ProductCard from "./ProductCard";
+import productsData from "../data/products";
+
+function ProductList({ addToCart }) {
+  return (
+    <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {productsData.map((product) => (
+        <ProductCard key={product.id} product={product} addToCart={addToCart} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
