dd2b03d60e24eebff3ab01e183595e460e3e7f7b
mupashirbare <mubashirbarre12@gmail.com>
2025-02-24 07:45:14 -0800

    i was added cardItem.

---
+++ src/Components/CartItem.jsx
@@ -0,0 +1,16 @@
+function CartItem({ product, removeFromCart }) {
+    return (
+      <div className="flex justify-between bg-white p-4 mb-2 shadow rounded-lg">
+        <span>{product.name} - ${product.price}</span>
+        <button
+          onClick={removeFromCart}
+          className="text-red-500 hover:underline"
+        >
+          Remove
+        </button>
+      </div>
+    );
+  }
+  
+  export default CartItem;
+  
